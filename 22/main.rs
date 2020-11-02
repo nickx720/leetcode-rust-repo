64b8a397f8d99a8f67dd87a4df99b5024dc4c296
Nick  Tom Thomas <nickbetty2000@yahoo.com>
2020-11-02 12:39:39 +0000

    Leetcode 22 fix

--- 22/main.rs
+++ 22/main.rs
@@ -4,4 +4,16 @@ impl Solution {
         backtrack(&mut output, "".to_string(), 0, 0, n);
         output
     }
+    fn backtrack(list: &mut Vec<String>, current_string: String, open: i32, close: i32, max: i32) {
+        if (current_string.len() as i32) == (max * 2) {
+            list.push(current_string);
+            return;
+        }
+        if open < max {
+            backtrack(list, current_string.clone() + "(", open + 1, close, max)
+        }
+        if close < open {
+            backtrack(list, current_string.clone() + ")", open, close + 1, max)
+        }
+    }
 }
