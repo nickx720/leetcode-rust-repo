2c16c0a1e954efcb47f5f6ad092321a868dda3e8
Nick  Tom Thomas <nickbetty2000@yahoo.com>
2021-01-17 17:56:05 -0500

    Leetcode 2

---
+++ 2/main.rs
@@ -0,0 +1,26 @@
+impl Solution {
+    fn add_two_numbers(
+        l1: Option<Box<ListNode>>,
+        l2: Option<Box<ListNode>>,
+    ) -> Option<Box<ListNode>> {
+        match (l1, l2) {
+            (None, None) => None,
+            (Some(n), None) | (None, Some(n)) => Some(n),
+            (Some(n1), Some(n2)) => {
+                let sum = n1.val + n2.val;
+                if sum < 10 {
+                    Some(Box::new(ListNode {
+                        val: sum,
+                        next: add_two_numbers(n1.next, n2.next),
+                    }))
+                } else {
+                    let carry = Some(Box::new(ListNode::new(1)));
+                    Some(Box::new(ListNode {
+                        val: sum - 10,
+                        next: add_two_numbers(add_two_numbers(carry, n1.next), n2.next),
+                    }))
+                }
+            }
+        }
+    }
+}
