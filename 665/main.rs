ee9fdbddd54026e124847394c27db71803408523
Nick  Tom Thomas <nickbetty2000@yahoo.com>
2021-01-25 08:08:58 -0500

    Comments for 665

--- 665/main.rs
+++ 665/main.rs
@@ -3,11 +3,13 @@ impl Solution {
         let mut count = 0;
         let mut nums = nums;
         for i in 1..nums.len() {
+            // Check if current element is less than previous element
             if nums[i] < nums[i - 1] {
+                // if current element is greater than [i-2], make the current element equal to previous element, to ensure increasing order
                 if i == 1 || nums[i - 2] <= nums[i] {
                     nums[i - 1] = nums[i];
                     count += 1;
-                } else {
+                } else { // current element is equal to previous element
                     nums[i] = nums[i - 1];
                     count += 1;
                 }
