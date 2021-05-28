08f22448f5fc588645efc8265359090f26f4b860
Nick  Tom Thomas <nickbetty2000@yahoo.com>
2021-05-28 08:54:15 -0400

    Calculate the permiter by adding 4 for each side, incase previous on the left or top is 1, decrement 2 as per permiter definition

---
+++ 463/main.rs
@@ -0,0 +1,19 @@
+impl Solution {
+    pub fn island_perimeter(grid: Vec<Vec<i32>>) -> i32 {
+        let mut perimeter = 0;
+    for i in 0..grid.len() {
+        for j in 0..grid[i].len() {
+            if grid[i][j] == 1 {
+                perimeter += 4;
+                if i != 0 && grid[i - 1][j] == 1  {
+                    perimeter -=2 ;
+                }
+                if j != 0 && grid[i][j - 1] == 1  {
+                    perimeter -= 2;
+                }
+            }
+        }
+    }
+    perimeter
+    }
+}
